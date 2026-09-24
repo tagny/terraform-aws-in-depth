48680e9b2d7a06862d3e2f5dfcec945081e579ba
tagnyngompe <tagny@ymail.com>
2026-09-24 06:02:32 +0000

    Add EC2 instance module with necessary configurations and outputs

---
+++ modules/ec2_instance/main.tf
@@ -0,0 +1,19 @@
+data "aws_ami" "ubuntu" {
+  # This is the AWS account for Cannonical, the makers of Ubuntu.
+  owners      = ["099720109477"]
+  most_recent = true
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "hello_world" {
+  ami           = data.aws_ami.ubuntu.id
+  subnet_id     = var.subnet_id
+  instance_type = var.instance_type
+}
