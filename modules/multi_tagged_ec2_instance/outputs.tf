1a5eed45e13ef7e192a6823ef3d07ebc0e044c7f
tagnyngompe <tagny@ymail.com>
2026-09-25 08:15:19 +0000

    Fix output name and description for instance resources in multi-tagged EC2 instance module

--- modules/multi_tagged_ec2_instance/outputs.tf
+++ modules/multi_tagged_ec2_instance/outputs.tf
@@ -6,7 +6,7 @@ output "aws_instance_ip" {
   description = "The IP Address for the private network interface on the instance."
   value       = aws_instance.hello_world.private_ip
 }
-output "aws_instance" {
-  description = "The entire instance resource."
+output "aws_instances" {
+  description = "The entire instance resources."
   value       = aws_instance.hello_world
 }
