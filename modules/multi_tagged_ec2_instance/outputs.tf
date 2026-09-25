feca11382ac7d889db07490d6fc255d1d324b453
tagnyngompe <tagny@ymail.com>
2026-09-25 08:20:24 +0000

    Refactor output variables for multi-tagged EC2 instances to support multiple instances

--- modules/multi_tagged_ec2_instance/outputs.tf
+++ modules/multi_tagged_ec2_instance/outputs.tf
@@ -1,10 +1,10 @@
-output "aws_instance_arn" {
-  description = "The AWS Resource Name for the instance."
-  value       = aws_instance.hello_world.arn
+output "aws_instance_arns" {
+  description = "The AWS Resource Name for the instances."
+  value       = [for instance in var.aws_instance.hello_world : instance.arn]
 }
-output "aws_instance_ip" {
-  description = "The IP Address for the private network interface on the instance."
-  value       = aws_instance.hello_world.private_ip
+output "aws_instance_ips" {
+  description = "The IP Address for the private network interface on the instances."
+  value       = [for instance in var.aws_instance.hello_world : instance.private_ip]
 }
 output "aws_instances" {
   description = "The entire instance resources."
