8487a2348474aa130a51886c443363c597ae71c8
tagnyngompe <tagny@ymail.com>
2026-09-26 11:03:45 +0000

    Add IAM role and instance profile for EC2 instances; update README with new inputs and outputs

--- modules/multi_tagged_ec2_instance/main.tf
+++ modules/multi_tagged_ec2_instance/main.tf
@@ -12,11 +12,33 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+data "aws_iam_policy_document" "instance_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "main" {
+  name               = "${var.name_prefix}-instance-role"
+  assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
+}
+
+resource "aws_iam_instance_profile" "main" {
+  name = aws_iam_role.main.name
+  role = aws_iam_role.main.name
+}
+
 resource "aws_instance" "hello_world" {
-  count         = var.instance_count
-  ami           = data.aws_ami.ubuntu.id
-  subnet_id     = var.subnet_id
-  instance_type = var.instance_type
+  count                = var.instance_count
+  ami                  = data.aws_ami.ubuntu.id
+  subnet_id            = var.subnet_id
+  instance_type        = var.instance_type
+  iam_instance_profile = aws_iam_instance_profile.main.name
 
   tags = merge(var.tags, {
     Name = "${var.name_prefix}-${count.index}"
